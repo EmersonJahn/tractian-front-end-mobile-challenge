7ecb134b634d675d1f78f061cd42c1993e3b4574
Emerson Jahn <emersonajahn@gmail.com>
2022-07-25 12:00:24 -0300

    feat: create a Header and remove unnecessary files

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { SafeArea } from "antd-mobile";
+
+import './Header.css'
+
+export default () => (
+    <div className="header">
+        <SafeArea position="top" />
+        TRACTIAN - FRONT-END - MOBILE CHALLENGE
+    </div>
+);
