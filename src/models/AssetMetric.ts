a794d289f2ef259bc01af20c6356240ac130538b
Emerson Jahn <emersonajahn@gmail.com>
2022-08-07 20:30:49 -0300

    feat: implement AssetDetails form

--- src/models/AssetMetric.ts
+++ src/models/AssetMetric.ts
@@ -1,12 +1,12 @@
 export class AssetMetric {
     private _totalCollectsUptime: number;
     private _totalUptime: number;
-    private _lastUptimeAt: string;
+    private _lastUptimeAt: Date;
 
     constructor (metrics: any) {
         this._totalCollectsUptime = metrics.totalCollectsUptime;
         this._totalUptime  = metrics.totalUptime;
-        this._lastUptimeAt = metrics.lastUptimeAt;
+        this._lastUptimeAt = new Date(metrics.lastUptimeAt);
     }
 
     public get totalCollectsUptime(): number {
@@ -17,7 +17,7 @@ export class AssetMetric {
         return this._totalUptime;
     }
 
-    public get lastUptimeAt(): string {
+    public get lastUptimeAt(): Date {
         return this._lastUptimeAt;
     }
 }
--- src/pages/assets/AssetDetails.tsx
+++ src/pages/assets/AssetDetails.tsx
@@ -1,20 +1,247 @@
-import { useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { Location } from 'history';
 
-import { Asset } from "../../models/Asset";
+import { Button, Form, Image, Input, List, Loading, Selector, Space, Tag } from 'antd-mobile';
+import { HeartFill } from 'antd-mobile-icons';
+
+import './AssetDetails.css'
+
+import { Asset } from '../../models/Asset';
+import fakeApi from '../../api/fake-api';
+import ResultBlock from '../../components/ResultBlock';
+import Header from '../../components/Header';
+import { User } from '../../models/User';
 
 export default () => {
-    const teste = useParams();
-    const location: any = useLocation();
-    const asset: Asset = location.state.asset;
+    const { companyId, unitId, assetId } = useParams();
+
+    const [asset, setAsset] = useState<Asset>();
+    const [users, setUsers] = useState<User[]>([]);
+
+    const [errorMessage, setErrorMessage] = useState<string>('');  
+    const [loading, setLoading] = useState<boolean>(false);
+    const [saving, setSaving] = useState<boolean>(false);
+    const [searched, setSearched] = useState<boolean>(false);
+    const [changed, setChanged] = useState<boolean>(false);
+
+    const [form] = Form.useForm();
     
     useEffect(() => {
-        console.log('teste', teste);
-        console.log('location', location);
-        console.log('asset', asset);
-    }, [])
+        getUsers();
+        getAsset();
+    }, [assetId]);
+
+    const getAsset = () => {
+        setLoading(true);
+
+        fakeApi.getAssetById(+assetId!)
+        .then(
+            asset => {
+                setAsset(asset);
+                setErrorMessage('');
+                setSearched(true); // para nao mostrar a mensagem de nao encontrado antes de buscar pela primeira vez
+                setLoading(false);
+            }
+        )
+        .catch(
+            error => {
+                console.error(error);
+                setErrorMessage(error.message);
+                setSearched(true); // para nao mostrar a mensagem de nao encontrado antes de buscar pela primeira vez
+                setLoading(false);
+            }
+        )
+    }
+
+    const getUsers = () => {
+        if (!companyId || !unitId) {
+            return;
+        }
+
+        fakeApi.getUsersWithByCompanyIdAndUnitId(+companyId, +unitId)
+        .then(
+            users => {
+                setUsers(users.data);// para nao mostrar a mensagem de nao encontrado antes de buscar pela primeira vez
+                setLoading(false);
+            }
+        )
+        .catch(
+            error => {
+                console.error(error);
+                setLoading(false);
+            }
+        )
+    }
+
+    const onFinish = () => {
+        console.log('finisih');
+        const n = form.getFieldValue('name');
+        console.log('n', n);
+        
+    }
 
     return (
-        <div>AssetDetails-{asset.name}</div>
+        <Space direction='vertical' className='main-box'>
+            <Header title={asset?.name ? asset.name : ''} goBackTo={`/${companyId}/${unitId}/assets/`} />
+            
+            <Space direction='vertical' align='center' justify='center' className='secondary-box'>
+                {loading ?
+                    <Loading color='primary'></Loading>
+                : !!errorMessage ?
+                    <ResultBlock 
+                        status='error'
+                        title={errorMessage}
+                    /> 
+                : !asset && searched ?
+                    <ResultBlock 
+                        status='info'
+                        title='Ativo não encontrado.'
+                    />
+                : asset ?
+                    <>
+                        <Image src={asset!.image} alt='Imagem do ativo' style={{padding: '24px'}} />
+                        <Form
+                            form={form}
+                            onFinish={onFinish}
+                            onValuesChange={() => setChanged(true)}
+                            initialValues={{
+                                name: asset.name,
+                                model: asset.model,
+                                healthscore: asset.healthscore,
+                                status: asset.status.status,
+                                inChargeId: asset.inChargeId,
+                                sensors: asset.sensors,
+                                metrics: asset.metrics,
+                                specifications: asset.specifications,
+                                maxTemp: asset.specifications.maxTemp,
+                                power: asset.specifications.power,
+                                rpm: asset.specifications.rpm,
+                            }}
+                            layout='vertical'
+                            footer={
+                                <Button block color='primary' type='submit' loading={saving} >
+                                    Salvar 
+                                </Button>
+                            }
+                        >
+                            <Form.Item 
+                                label='Nome'
+                                name='name'
+                                rules={[{ required: true, message: 'Nome deve ser informado.' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item 
+                                label='Modelo'
+                                name='model'
+                                rules={[{ required: true, message: 'Modelo deve ser informado.' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item 
+                                label={<>Nivel de saude <HeartFill color='red' /></>}
+                                name='healthscore'
+                                rules={[{ required: true, message: 'Nível de saúde deve ser informado.' }]}
+                            >
+                                <Input type='number' />
+                            </Form.Item>
+                            <Form.Item
+                                label='Status'
+                                name='status'
+                                rules={[{ required: true, message: 'Status deve ser selecionado.' }]}
+                            >
+                                <Selector 
+                                    columns={3}
+                                    options={[
+                                        {label: 'Em Alerta', value: 'inAlert'},
+                                        {label: 'Em Operação', value: 'inOperation'},
+                                        {label: 'Em Parada', value: 'inDowntime'},
+    
+                                    ]}
+                                    defaultValue={[asset.status.status]}
+                                />
+                            </Form.Item>
+                            <Form.Item
+                                label='Responsável'
+                                name='inCharge'
+                            >
+                                <Selector 
+                                    columns={3}
+                                    options={
+                                        users.map((user: User) => (
+                                            {label: user.name, value: user.id}
+                                        ))
+                                    }
+                                    defaultValue={asset.inChargeId ? [asset.inChargeId] : []}
+                                />
+                            </Form.Item>
+                            <Form.Item 
+                                label='Temperatura máxima (°C)'
+                                name='maxTemp'
+                            >
+                                <Input type='number' />
+                            </Form.Item>
+                            <Form.Item 
+                                label='Potência (kWh)'
+                                name='power'
+                            >
+                                <Input type='number' />
+                            </Form.Item>
+                            <Form.Item 
+                                label='RPM'
+                                name='rpm'
+                            >
+                                <Input type='number' />
+                            </Form.Item>
+                            <Form.Item 
+                                label='Sensores'
+                                name='sensors'
+                            >
+                                {asset.sensors.map((sensor: string, index: number) => (
+                                    <Tag color='primary'>{sensor}</Tag>
+                                ))}
+                                {/* <List>
+                                    {asset.sensors.map((sensor: string, index: number) => (
+                                        <List.Item 
+                                            key={index} 
+                                            className='asset-item'
+                                        >
+                                            {sensor}
+                                        </List.Item>
+                                    ))}
+                                </List> */}
+                            </Form.Item>
+                            {/* {Object.keys(asset.specifications).length > 0 ?
+                                <Form.Item
+                                    label='Especificações'
+                                    name='specifications'
+                                    className='specifications'
+                                >
+                                    {asset.specifications.maxTemp ? 
+                                        <p>Temperatura máxima: {asset.specifications.maxTemp} °C</p>
+                                    : null}
+                                    {asset.specifications.power ? 
+                                        <p>Potência: {asset.specifications.power} kWh</p>
+                                    : null}
+                                    {asset.specifications.rpm ? 
+                                        <p>RPM: {asset.specifications.rpm}</p>
+                                    : null}                
+                                </Form.Item>
+                            : null} */}
+                            <Form.Item
+                                label='Métricas'
+                                name='metrics'
+                                className='metrics'
+                            >
+                                <p><span>Total de coletas: </span>{asset.metrics.totalCollectsUptime}</p>
+                                <p><span>Total de horas de coletas: </span>{asset.metrics.totalUptime.toFixed()}</p>
+                                <p><span>Última coleta: </span>{asset.metrics.lastUptimeAt.toLocaleString()}</p>
+                            </Form.Item>
+                        </Form>
+                    </>
+                : null}
+            </Space>
+        </Space>
     );
 }
