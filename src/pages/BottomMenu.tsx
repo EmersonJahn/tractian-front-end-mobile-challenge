9e892c8e1d639e2bdb575c74891fdcee5c808eca
Emerson Jahn <emersonajahn@gmail.com>
2022-08-09 19:07:21 -0300

    fix: BottomMenu showing wrong tab on return of UserDetails

--- src/pages/BottomMenu.tsx
+++ src/pages/BottomMenu.tsx
@@ -6,17 +6,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 import './BottomMenu.css';
+import { useEffect, useState } from "react";
 
 export default () => {
     const navigate = useNavigate();
     const location = useLocation();
-    // const { companyId, unitId } = useParams();
 
-    const showMenu: boolean = location.pathname.endsWith("assets/") || location.pathname.endsWith("dashboard/") || location.pathname.endsWith("users/")
+    const showMenu: boolean = location.pathname.endsWith("assets/") || location.pathname.endsWith("dashboard/") || location.pathname.endsWith("users/");
+    const path: string = location.pathname.endsWith("dashboard/") ? "dashboard/" : location.pathname.endsWith("users/") ? "users/" : "assets/"; // para determinar o defaultActiveKey
 
     const naviteTo = (value: string) => {
         const companyId = location.pathname.split("/")[1];
         const unitId = location.pathname.split("/")[2];        
+
         navigate(`/${companyId}/${unitId}/${value}`);
     }
     
@@ -40,7 +42,7 @@ export default () => {
 
     return (
         showMenu ? 
-            <TabBar onChange={naviteTo} className="tabsBar">
+            <TabBar onChange={naviteTo} className="tabsBar" defaultActiveKey={path}>
                 {tabs.map(item => (
                     <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
                 ))}
